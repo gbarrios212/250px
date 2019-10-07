41ba613a2359e5cf3b0d14bddb2b8eade3654cb8
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-07 17:52:57 -0400

    Add store

--- fullstack250px/frontend/250px.jsx
+++ fullstack250px/frontend/250px.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
-//test
+//test ajax
 // import { login, signup, logout } from './util/session_api_util'
 // window.login = login;
 // window.signup = signup;
 // window.logout = logout;
+
+//test store
+// const store = configureStore();
+// window.getState = store.getState;
+// window.dispatch = store.dispatch;
 //
 
 
