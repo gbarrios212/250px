8dc7aea839cee8f4043eb99f37bd80bd7eebd3ea
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-07 16:35:32 -0400

    Add sessionReducer

--- fullstack250px/frontend/250px.jsx
+++ fullstack250px/frontend/250px.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//test
+// import { login, signup, logout } from './util/session_api_util'
+// window.login = login;
+// window.signup = signup;
+// window.logout = logout;
+//
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     ReactDOM.render(<h1>REPLACING ROOT</h1>, root)
---
+++ fullstack250px/frontend/reducers/session_reducer.js
@@ -0,0 +1,24 @@
+import { 
+    RECEIVE_CURRENT_USER, 
+    LOGOUT_CURRENT_USER 
+} from '../actions/session_actions';
+
+const _nullState = {
+    id: null
+}
+
+const sessionReducer = (oldState = _nullState, action) => {
+    Object.freeze(oldState);
+    switch(action.type){
+        case RECEIVE_CURRENT_USER:
+            let newState = Object.assign({}, oldState);
+            oldState[id] = action.user.id;
+            return newState
+        case LOGOUT_CURRENT_USER:
+            return _nullState;
+        default: 
+            return oldState;
+    }
+}
+
+export default sessionReducer;
