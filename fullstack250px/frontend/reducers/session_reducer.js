3be0e7291670938a8ebfe4041bddea4475428133
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-07 19:32:02 -0400

    Refactor sessionReducer

--- fullstack250px/frontend/reducers/session_reducer.js
+++ fullstack250px/frontend/reducers/session_reducer.js
@@ -11,6 +11,7 @@ const sessionReducer = (oldState = _nullState, action) => {
     Object.freeze(oldState);
     switch(action.type){
         case RECEIVE_CURRENT_USER:
+            // debugger;
             return {
                 id: action.user.id
             }
