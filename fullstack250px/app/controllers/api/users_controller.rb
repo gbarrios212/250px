d424d17a5b96ae9bb5b1f3459599ad4001c7a165
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-07 15:07:54 -0400

    Add jbuilder

---
+++ fullstack250px/app/controllers/api/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::UsersController < ApplicationController
+    def create 
+        @user = User.new(user_params)
+
+        if @user.save 
+            login(@user)
+            #add login method 
+            render 'api/users/show'
+        else 
+            render json: @user.errors.full_messages
+    end
+
+    # def show 
+    # end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+end
