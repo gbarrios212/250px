5d9fbe765e9b33d937e3dea31aae3f6a3598a220
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-09 22:25:45 -0400

    Add photos model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,20 @@
-class Photo < ApplicationRecord
-    
+class Photo < ApplicationRecord
+    validates :username, :email, :password_digest, :session_token, presence: true
+    validates :username, :email, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+
+    belongs_to :author,
+        class_name: :User
+    # has_many :likes
+    # has_many :comments
+    # has_many :galleries
+    # has_many :follows,
+        # through: :author,
+        # source: :followers 
+    # has_many :photo_galleries
+        # foreign_key: :photo_id. 
+        # class_name: :Galleries_photos check this !!!!
+    # has_many :galleries,
+        # through: :photo_galleries
+        # source: :galleries check this!!!!
 end
