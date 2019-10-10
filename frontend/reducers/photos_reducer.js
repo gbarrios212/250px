5049852ccfa9622c7a6a69a33a432d64ba5ba7af
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-10 13:00:42 -0400

    Update photos reducer

--- frontend/reducers/photos_reducer.js
+++ frontend/reducers/photos_reducer.js
@@ -1,11 +1,19 @@
-import { RECEIVE_ALL_PHOTOS } from "../actions/photo_actions";
-
+import { RECEIVE_ALL_PHOTOS, RECEIVE_PHOTO, REMOVE_PHOTO } from "../actions/photo_actions";
+import merge from 'lodash/merge';
 
 const photosReducer = (state = {}, action) => {
     Object.freeze(state);
+    let newState;
     switch(action.type){
         case RECEIVE_ALL_PHOTOS:
             return action.photos;
+        case RECEIVE_PHOTO:
+            newState = merge({}, state, {[action.post.id]: action.post});
+            return newState;
+        case REMOVE_PHOTO:
+            newState = merge({}, state)
+            delete newState[action.id];
+            return newState;
         default: 
             return state;
     }
