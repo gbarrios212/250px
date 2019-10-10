4cfa4ddbdb84574c67a61fff9c3f0f44f1dba148
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-10 10:22:16 -0400

    Add photos index, photos index container

---
+++ frontend/components/photos/photos_index_container.js
@@ -0,0 +1,13 @@
+import PhotosIndex from './photos_index';
+import { connect } from 'react-redux';
+import fetchPhotos from '../../actions/photo_actions';
+
+const msp = (state) => ({
+    photos: Object.values(state.entities.photos)
+})
+
+const mdp = (dispatch) => ({
+    fetchPhotos: () => dispatch(fetchPhotos())
+})
+
+export default connect(msp, mdp)(PhotosIndex)
