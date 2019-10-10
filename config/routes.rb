19c0dec857d11253f742f1d3c3eb8b521ba3d610
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-10 12:27:59 -0400

    Nest photo resources under user

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do 
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create, :show] do 
+      resources :photos, only: [:create, :update, :delete]
+    end
     resource :session, only: [:create, :destroy]
-    resources :photos, only: [:index, :show, :create]
+    resources :photos, only: [:index, :show]
   end
 end
