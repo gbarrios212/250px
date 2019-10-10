f39e1b929b9fbcda631ae4e915d00b6c1e129ab1
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-10 10:30:54 -0400

    Add photosIndexContainer to app under /

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,6 +5,7 @@ import SignUpFormContainer from './session/signup_form_container';
 import LoginFormContainer from './session/login_form_container';
 import { AuthRoute } from '../util/route_util';
 import SplashContainer from './splash/splash_container';
+import PhotosIndexContainer from './photos/photos_index_container';
 
 
 const App = () => (
@@ -15,6 +16,7 @@ const App = () => (
       <AuthRoute path="/signup" component={SignUpFormContainer}/>
       <AuthRoute path="/login" component={LoginFormContainer}/>
       <Route exact path="/" component={SplashContainer} />
+      <Route exaxt path="/" component={PhotosIndexContainer} />
   </>
 );
 
