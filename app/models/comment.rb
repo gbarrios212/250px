f8959b2ca6c520a49c20253aa8c785656465c987
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-13 00:41:05 -0400

    Add comment model level validations, associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+    validates :body, :author_id, :photo_id, presence: true
+
+    belongs_to :author, 
+        class_name: :User 
+    belongs_to :photo
+    belongs_to :parent_comment, 
+        class_name: :Comment
 end
