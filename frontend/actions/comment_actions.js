82dbfd5ab968fcf2fda09c6c5786f526156f2e4e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-13 14:18:29 -0400

    Add comment, like actions

---
+++ frontend/actions/comment_actions.js
@@ -0,0 +1,40 @@
+import * as CommentApiUtil from '../util/comment_api_util'
+
+export const RECEIVE_ALL_COMMENTS = 'RECEIVE_ALL_COMMENTS';
+export const RECEIVE_COMMENT = 'RECEIVE_COMMENT';
+export const REMOVE_COMMENT = 'REMOVE_COMMENT';
+
+const receiveAllComments = (comments) => ({
+    type: RECEIVE_ALL_COMMENTS,
+    comments
+})
+
+const receiveComment = (comment) => ({
+    type: RECEIVE_COMMENT,
+    comment
+})
+
+const removeComment = (commentId) => ({
+    type: REMOVE_COMMENT,
+    commentId
+})
+
+export const fetchComments = (photoId) => (dispatch) => {
+    return CommentApiUtil.fetchComments(photoId)
+        .then(comments => dispatch(receiveAllComments(comments)));
+}
+
+export const createComment = (comment) => (dispatch) => {
+    return CommentApiUtil.createComment(comment)
+        .then(comment => dispatch(receiveComment(comment)), err => dispatch(receiveErrors(err.responseJSON)));
+}
+
+export const updateComment = (comment) => (dispatch) => {
+    return CommentApiUtil.updateComment(comment)
+        .then(comment => dispatch(receiveComment(comment)), err => dispatch(receiveErrors(err.responseJSON)));
+}
+
+export const deleteComment = (commentId) => (dispatch) => {
+    return CommentApiUtil.deleteComment(commentId)
+        .then(() => dispatch(removeComment(commentId)));
+}
---
+++ frontend/actions/like_actions.js
@@ -0,0 +1,25 @@
+import * as LikeApiUtil from '../util/likes_api_util'
+
+export const RECEIVE_LIKE = 'RECEIVE_LIKE';
+export const REMOVE_LIKE = 'REMOVE_LIKE';
+
+const receiveLike = (like) => ({
+    type: RECEIVE_LIKE,
+    like
+})
+
+const removeLike = (likeId) => ({
+    type: REMOVE_LIKE,
+    likeId
+})
+
+export const createLike = (like) => (dispatch) => {
+    return LikeApiUtil.createLike(like)
+        .then(like => dispatch(receiveLike(like)), err => dispatch(receiveErrors(err.responseJSON)));
+}
+
+
+export const deleteLike = (likeId) => (dispatch) => {
+    return LikeApiUtil.deleteLike(likeId)
+        .then(() => dispatch(removeLike(like)), err => dispatch(receiveErrors(err.responseJSON)));
+}
