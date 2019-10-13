226d909ec3f4dca6a24a21975a17e8fd8e284f2d
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-12 21:18:07 -0400

    Add outline to selected manage photos

--- frontend/components/manage/manage_library_detail.jsx
+++ frontend/components/manage/manage_library_detail.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-const ManageLibraryDetail= ({photo}) => {
-    return(
-        <div>
-            <img src={photo.photoUrl} alt=""></img>
-        </div>
-    )
+class ManageLibraryDetail extends React.Component{
+    constructor(props) {
+        super(props)
+
+        this.state={ selected: false };
+        this.toggleSelect = this.toggleSelect.bind(this);
+    }
+
+    toggleSelect(e) {
+        const currentSelect = this.state.selected;
+        this.setState({ selected: !currentSelect });
+    }
+
+
+    render(){
+        return(
+            <div>
+                <img onClick={this.toggleSelect} className={this.state.selected ? 'thumbnail selected' : 'thumbnail'} src={this.props.photo.photoUrl} alt=""></img>
+            </div>
+        )
+    }
     
 }
 
