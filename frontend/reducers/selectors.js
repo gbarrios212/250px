fd4215f7fdd1478f0c855ef0b113b75d1701ef36
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-14 19:01:15 -0400

    Fix show page render issue; add filter to selector for comments

--- frontend/reducers/selectors.js
+++ frontend/reducers/selectors.js
@@ -1,14 +1,10 @@
 export const selectUserPhotos = (state, user) => {
-    // debugger;
-    // if (!state.entities.photos[0]){
-    //     return [];
-    // }
-    debugger;
     let photos = user ? user.photo_ids.map(id => state.entities.photos[id]) : [];
     return photos.filter(photo => photo !== undefined );
 };
   
 export const selectPhotoComments = (state, photo) => {
     // debugger;
-    return photo ? photo.comment_ids.map(id => state.entities.comments[id]) : [];
+    let comments = photo ? photo.comment_ids.map(id => state.entities.comments[id]) : [];
+    return comments.filter(comment => comment !== undefined );
 }
