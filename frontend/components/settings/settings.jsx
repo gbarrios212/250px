83ce430ad47d70224ddbbc85e6790d60fd903b52
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-15 18:44:20 -0400

    Add settings containers

--- frontend/components/settings/settings.jsx
+++ frontend/components/settings/settings.jsx
@@ -7,7 +7,14 @@ class Settings extends React.Component{
 
     render (){
         return(
-            <div>Settings</div>
+            <div className="settings-container">Settings
+                <div className="personal-info">
+                    
+                </div>
+                <div className="profile-info">
+
+                </div>
+            </div>
         )
     }
 }
