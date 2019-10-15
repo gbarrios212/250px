7b3aa1de2f5141c3f4d3b185d0abc82a5e9b759c
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-15 00:41:50 -0400

    Restyle modal image preview area

--- frontend/components/photos/photos_form.jsx
+++ frontend/components/photos/photos_form.jsx
@@ -73,72 +73,77 @@ class PhotosForm extends React.Component {
             <div></div>
         );
         
-        const preview = this.state.photoUrl ? <img src={this.state.photoUrl} /> : null;
+        const preview = this.state.photoUrl ? <img className="preview" src={this.state.photoUrl} /> : null;
         return (
-            <form className="create_form" onSubmit={this.handleSubmit}>
-                {errorsList}
-                <br/>
-                <label htmlFor="file">Choose File:</label>
-                <input id="file" type="file" onChange={this.handleFile} />
-                <h3>Image Preview</h3>
-                {preview}
-                <label htmlFor="name">Name:</label>
-                <input id="name" type="text" onChange={this.update("name")} value={this.state.name}/>
-                <label htmlFor="description">Description</label>
-                <input id="description" type="text" onChange={this.update("description")} value={this.state.description}/>
-                
-                <label htmlFor="category">Category:</label>
-                <select id="category" name="category" type="text" onChange={this.update("category")} value={this.state.category}>
-                    <option value="uncategorized">Uncategorized</option>
-                    <option value="abstract">Abstract</option>
-                    <option value="aerial">Aerial</option>
-                    <option value="animals">Animals</option>
-                    <option value="black and white">Black and White</option>
-                    <option value="celebrities">Celebrities</option>
-                    <option value="city and architecture">City & Architecture</option>
-                    <option value="commercial">Commercial</option>
-                    <option value="concert">Concert</option>
-                    <option value="family">Family</option>
-                    <option value="fashion">Fashion</option>
-                    <option value="film">Film</option>
-                    <option value="fine art">Fine Art</option>
-                    <option value="food">Food</option>
-                    <option value="journalism">Journalism</option>
-                    <option value="landscapes">Landscapes</option>
-                    <option value="macro">Macro</option>
-                    <option value="nature">Nature</option>
-                    <option value="night">Night</option>
-                    <option value="nude">Nude</option>
-                    <option value="people">People</option>
-                    <option value="performing arts">Performing Arts</option>
-                    <option value="sport">Sport</option>
-                    <option value="still life">Still Life</option>
-                    <option value="street">Street</option>
-                    <option value="transportation">Transportation</option>
-                    <option value="travel">Travel</option>
-                    <option value="underwater">Underwater</option>
-                    <option value="urban exploration">Urban Exploration</option>
-                    <option value="wedding">Wedding</option>
-                </select>
+            <div className="modal-container">
+                <div className="image-preview-area">
+                <h3 className="image-preview-header">Image Preview</h3>
+                    {preview}
+                </div>
+                <form className="create-form" onSubmit={this.handleSubmit}>
+                    {errorsList}
+                    <br/>
+                    <label htmlFor="file">Choose File:</label>
+                    <input id="file" type="file" onChange={this.handleFile} />
+                    
+                    <label htmlFor="name">Name:</label>
+                    <input id="name" type="text" onChange={this.update("name")} value={this.state.name}/>
+                    <label htmlFor="description">Description</label>
+                    <input id="description" type="text" onChange={this.update("description")} value={this.state.description}/>
+                    
+                    <label htmlFor="category">Category:</label>
+                    <select id="category" name="category" type="text" onChange={this.update("category")} value={this.state.category}>
+                        <option value="uncategorized">Uncategorized</option>
+                        <option value="abstract">Abstract</option>
+                        <option value="aerial">Aerial</option>
+                        <option value="animals">Animals</option>
+                        <option value="black and white">Black and White</option>
+                        <option value="celebrities">Celebrities</option>
+                        <option value="city and architecture">City & Architecture</option>
+                        <option value="commercial">Commercial</option>
+                        <option value="concert">Concert</option>
+                        <option value="family">Family</option>
+                        <option value="fashion">Fashion</option>
+                        <option value="film">Film</option>
+                        <option value="fine art">Fine Art</option>
+                        <option value="food">Food</option>
+                        <option value="journalism">Journalism</option>
+                        <option value="landscapes">Landscapes</option>
+                        <option value="macro">Macro</option>
+                        <option value="nature">Nature</option>
+                        <option value="night">Night</option>
+                        <option value="nude">Nude</option>
+                        <option value="people">People</option>
+                        <option value="performing arts">Performing Arts</option>
+                        <option value="sport">Sport</option>
+                        <option value="still life">Still Life</option>
+                        <option value="street">Street</option>
+                        <option value="transportation">Transportation</option>
+                        <option value="travel">Travel</option>
+                        <option value="underwater">Underwater</option>
+                        <option value="urban exploration">Urban Exploration</option>
+                        <option value="wedding">Wedding</option>
+                    </select>
 
-                <label htmlFor="location">Location:</label>
-                <input id="location" type="text" onChange={this.update("location")} value={this.state.location}/>
-                <label htmlFor="date_taken">Date Taken:</label>
-                <input id="date_taken" type="text" onChange={this.update("date_taken")} value={this.state.date_taken}/>
-                <label htmlFor="camera">Camera:</label>
-                <input id="camera" type="text" onChange={this.update("camera")} value={this.state.camera}/>
-                <label htmlFor="lens">Lens:</label>
-                <input id="lens" type="text" onChange={this.update("lens")} value={this.state.lens}/>
-                <label htmlFor="focal_length">Focal Length:</label>
-                <input id="focal_length" type="text" onChange={this.update("focal_length")} value={this.state.focal_length}/>
-                <label htmlFor="aperture">Aperture:</label>
-                <input id="aperture" type="text" onChange={this.update("aperture")} value={this.state.aperture}/>
-                <label htmlFor="shutter_speed">Shutter Speed:</label>
-                <input id="shutter_speed" type="text" onChange={this.update("shutter_speed")} value={this.state.shutter_speed}/>
-                <label htmlFor="iso">ISO:</label>
-                <input id="iso" type="text" onChange={this.update("iso")} value={this.state.iso}/>
-                <button className="create-button">Create that Cat!</button>
-            </form>
+                    <label htmlFor="location">Location:</label>
+                    <input id="location" type="text" onChange={this.update("location")} value={this.state.location}/>
+                    <label htmlFor="date_taken">Date Taken:</label>
+                    <input id="date_taken" type="text" onChange={this.update("date_taken")} value={this.state.date_taken}/>
+                    <label htmlFor="camera">Camera:</label>
+                    <input id="camera" type="text" onChange={this.update("camera")} value={this.state.camera}/>
+                    <label htmlFor="lens">Lens:</label>
+                    <input id="lens" type="text" onChange={this.update("lens")} value={this.state.lens}/>
+                    <label htmlFor="focal_length">Focal Length:</label>
+                    <input id="focal_length" type="text" onChange={this.update("focal_length")} value={this.state.focal_length}/>
+                    <label htmlFor="aperture">Aperture:</label>
+                    <input id="aperture" type="text" onChange={this.update("aperture")} value={this.state.aperture}/>
+                    <label htmlFor="shutter_speed">Shutter Speed:</label>
+                    <input id="shutter_speed" type="text" onChange={this.update("shutter_speed")} value={this.state.shutter_speed}/>
+                    <label htmlFor="iso">ISO:</label>
+                    <input id="iso" type="text" onChange={this.update("iso")} value={this.state.iso}/>
+                    <button className="create-button">Create that Cat!</button>
+                </form>
+            </div>
         );
     }
 }
