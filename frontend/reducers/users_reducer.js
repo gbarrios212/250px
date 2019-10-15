0eb388f926fe0d1a4b57093118b8f5a1da7b97be
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-14 22:35:48 -0400

    Clean photo and users reducer

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -25,21 +25,13 @@ const usersReducer = (oldState = {}, action) => {
             
             newState[photo.author_id].photo_ids.push(photo.id);
             return newState;
-        //the below pushes the comment a user made to their authored_comment_ids
-        //this will be useful when severing a link - this must be on both the user and photo tables, 
-            //as well as the comments table
         case RECEIVE_COMMENT:
-            // debugger;
             comment = action.comment;
             newState = Object.assign({}, oldState);
             user = newState[comment.author_id]
             user.authored_comment_ids.push(comment.id);
             user.commented_photo_ids.push(comment.photo_id);
             return newState
-        // case REMOVE_PHOTO: 
-        // case REMOVE_COMMENT: 
-
-        //the following correspond to user - liked photo relationship 
         case RECEIVE_LIKE:
             like = action.like; 
             newState = Object.assign({}, oldState);
@@ -50,16 +42,9 @@ const usersReducer = (oldState = {}, action) => {
             photoId = action.payload.like.photo_id;
             userId = action.payload.like.user_id;
             user = action.payload.users[userId];
-            // debugger;
             user.liked_photo_ids = user.liked_photo_ids.filter(id => id !== photoId);
-            // delete action.payload.users[userId].liked_photo_ids[photoId];
-            debugger;
-            window.merge = merge;
             newState = merge({}, oldState, action.payload.users);
-            debugger;
-            // test below 
             newState[userId].liked_photo_ids = user.liked_photo_ids
-            // test above
             return newState;
         default: 
             return oldState;
