93b6814dbfd991d68573f188ca74e5778452b0a2
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-14 21:46:59 -0400

    Enable multiple edits on photos

--- frontend/components/photos/photo_edit_form.jsx
+++ frontend/components/photos/photo_edit_form.jsx
@@ -70,11 +70,11 @@ class PhotoEditForm extends React.Component {
           {errorsList}
           <label htmlFor="name">Name</label>
           <input id="name" type="text" onChange={this.update("name")} value={this.state.photo.name} disabled={this.state.disabled}/>
-          {/* <label htmlFor="description">Description</label>
-          <textarea id="description" type="text" onChange={this.update("description")} value={this.props.photo.description} placeholder="Tell us more about your photo" disabled={this.state.disabled}/>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" type="text" onChange={this.update("description")} value={this.state.photo.description} placeholder="Tell us more about your photo" disabled={this.state.disabled}/>
 
           <label htmlFor="category">Category</label>
-          <select id="category" name="category" type="text" onChange={this.update("category")} value={this.props.photo.category} disabled={this.state.disabled}>
+          <select id="category" name="category" type="text" onChange={this.update("category")} value={this.state.photo.category} disabled={this.state.disabled}>
                     <option value="uncategorized">Uncategorized</option>
                     <option value="abstract">Abstract</option>
                     <option value="aerial">Aerial</option>
@@ -108,24 +108,24 @@ class PhotoEditForm extends React.Component {
                 </select>
 
           <label htmlFor="location">Location</label>
-          <input id="location" type="text" onChange={this.update("location")} value={this.props.photo.location} disabled={this.state.disabled}/>
+          <input id="location" type="text" onChange={this.update("location")} value={this.state.photo.location} disabled={this.state.disabled}/>
           <label htmlFor="date_taken">Date Taken</label>
-          <input id="date_taken" type="text" onChange={this.update("date_taken")} value={this.props.photo.date_taken} placeholder="(dd/mm/yyyy)" disabled={this.state.disabled}/>
+          <input id="date_taken" type="text" onChange={this.update("date_taken")} value={this.state.photo.date_taken} placeholder="(dd/mm/yyyy)" disabled={this.state.disabled}/>
           
-          <label>Camera Info</label> */}
+          <label>Camera Info</label>
           {/* <span className="cam-info"> */}
-            {/* <label htmlFor="camera"><p>Camera</p></label>
-            <input id="camera" type="text" onChange={this.update("camera")} value={this.props.photo.camera} disabled={this.state.disabled}/>
+            <label htmlFor="camera"><p>Camera</p></label>
+            <input id="camera" type="text" onChange={this.update("camera")} value={this.state.photo.camera} disabled={this.state.disabled}/>
             <label htmlFor="lens"><p>Lens</p></label>
-            <input id="lens" type="text" onChange={this.update("lens")} value={this.props.photo.lens} disabled={this.state.disabled}/>
+            <input id="lens" type="text" onChange={this.update("lens")} value={this.state.photo.lens} disabled={this.state.disabled}/>
             <label htmlFor="focal_length"><p>Focal Length</p></label>
-            <input id="focal_length" type="text" onChange={this.update("focal_length")} value={this.props.photo.focal_length} disabled={this.state.disabled}/>
+            <input id="focal_length" type="text" onChange={this.update("focal_length")} value={this.state.photo.focal_length} disabled={this.state.disabled}/>
             <label htmlFor="aperture"><p>Aperture</p></label>
-            <input id="aperture" type="text" onChange={this.update("aperture")} value={this.props.photo.aperture} disabled={this.state.disabled}/>
+            <input id="aperture" type="text" onChange={this.update("aperture")} value={this.state.photo.aperture} disabled={this.state.disabled}/>
             <label htmlFor="shutter_speed"><p>Shutter Speed</p></label>
-            <input id="shutter_speed" type="text" onChange={this.update("shutter_speed")} value={this.props.photo.shutter_speed} disabled={this.state.disabled}/>
+            <input id="shutter_speed" type="text" onChange={this.update("shutter_speed")} value={this.state.photo.shutter_speed} disabled={this.state.disabled}/>
             <label htmlFor="iso"><p>ISO</p></label>
-            <input id="iso" type="text" onChange={this.update("iso")} value={this.props.photo.iso} disabled={this.state.disabled}/> */}
+            <input id="iso" type="text" onChange={this.update("iso")} value={this.state.photo.iso} disabled={this.state.disabled}/>
           {/* </span> */}
           
           <button className="edit-button" disabled={this.state.disabled}>Change that Cat!</button>
