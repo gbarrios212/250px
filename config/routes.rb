90d108b9078d09de91c2474b48b8f231e3f31a3e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-10-16 18:00:15 -0400

    Fix routing syntax errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do 
     resources :users, only: [:create, :show, :update, :index] do 
       resources :follows, only: [:create]
+    end
     resource :session, only: [:create, :destroy]
     resources :photos, only: [:index, :show, :create, :update, :destroy] do 
       resources :likes, only: [:create]
       resources :comments, only: [:index, :create]
     end
-    # resources :likes, only: [:destroy]
     resources :comments, only: [:update, :destroy]
-    delete 'follows/:user_id', to 'follows#destroy', as 'follows'
+
+    delete 'follows/:user_id', to: 'follows#destroy', as: 'follows'
     delete 'likes/:photo_id', to: 'likes#destroy', as: 'likes'
   end
 end
