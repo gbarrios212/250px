f96f8c3b75f87943d9c0c43427e557030f0730db
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-12-16 15:25:37 -0500

    Add followed and featured section

--- frontend/components/photos/photos_index.jsx
+++ frontend/components/photos/photos_index.jsx
@@ -14,16 +14,40 @@ class PhotosIndex extends React.Component{
 
     render() {
         //  ;
+        let followList;
+        let followSection;
+        if (this.props.followPhotos.length > 0) {
+            let sortedFollowPhotos = this.props.followPhotos.sort((a, b) => (a.like_ids.length < b.like_ids.length) ? 1 : -1);
+            followList = sortedFollowPhotos.map(photo => {
+                return <PhotosIndexItem photo={photo} key={photo.id * 1000} />
+            })
+            followSection = (
+                <div className="follow-section">
+                    <h1 className="follow-section-header"> Users you like </h1> 
+                    {followList}
+                </div>
+            )
+        }
         let sorted = this.props.photos.sort((a, b) => (a.like_ids.length < b.like_ids.length) ? 1 : -1);
         let photosList = sorted.map(photo => {
             return <PhotosIndexItem photo={photo} key={photo.id}/>
         })
+        let photoSection = (
+            <div className="photo-section">
+                <h1 className="photo-section-header"> Featured </h1>
+                {photosList}
+            </div>
+        )
+
         return (
             <div className="photos-index-container">
                 {/* <div className="home-feed-text">Home Feed</div> */}
                 <div className="home-feed-text">The Roost</div>
+                <ul className="photos-index-3">
+                    {followSection}
+                </ul>
                 <ul className="photos-index-2">
-                    {photosList}
+                    {photoSection}
                 </ul>
             </div>
         )
