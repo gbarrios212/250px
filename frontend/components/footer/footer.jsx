e55504c17a3a062f2988d973f21e247fec573de6
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-12-17 09:43:17 -0500

    Fix comment display css

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,8 +9,8 @@ class Footer extends React.Component {
                     <div>Created by Gabriel Barrios</div>
                     <a href="https://github.com/gbarrios212" className="devLink"><i className="fab fa-github devLink"> GitHub</i></a>
                     <a href="https://www.linkedin.com/in/gabriel-antonio-barrios" className="devLink"><i className="fab fa-linkedin devLink"> LinkedIn</i></a>
-                    <a href="https://angel.co/gabriel-barrios-2" class="devLink"><i class="fab fa-angellist devLink"></i>    AngelList</a>
-                    <a href="https://gbarrios.com/" class="devLink"><img src="app/assets/images/G.svg" class="gb" />    Portfolio</a>
+                    <a href="https://angel.co/gabriel-barrios-2" className="devLink"><i className="fab fa-angellist devLink"></i>    AngelList</a>
+                    <a href="https://gbarrios.com/" className="devLink"><img src="app/assets/images/G.svg" className="gb" />    Portfolio</a>
                 </div>
             </div>
     )
