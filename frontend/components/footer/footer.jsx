38b7f1607a35296dae2d79df65cc2975ecdfe10e
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-12-17 10:02:07 -0500

    Fix footer portfolio logo css

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -10,7 +10,7 @@ class Footer extends React.Component {
                     <a href="https://github.com/gbarrios212" className="devLink"><i className="fab fa-github devLink"> GitHub</i></a>
                     <a href="https://www.linkedin.com/in/gabriel-antonio-barrios" className="devLink"><i className="fab fa-linkedin devLink"> LinkedIn</i></a>
                     <a href="https://angel.co/gabriel-barrios-2" className="devLink"><i className="fab fa-angellist devLink"></i>    AngelList</a>
-                    <a href="https://gbarrios.com/" className="devLink"><img src="app/assets/images/G.svg" className="gb" />    Portfolio</a>
+                    <a href="https://gbarrios.com/" className="devLink2"><div className="portfolio-logo"></div>Portfolio</a>
                 </div>
             </div>
     )
