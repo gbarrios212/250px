ca3eca32a81486154ff2234e7f0ad178efd0472c
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-12-17 16:39:08 -0500

    Normalize spacing and font in footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -7,8 +7,8 @@ class Footer extends React.Component {
             <div className="footer-container-outer">
                 <div className="footer-container-inner">
                     <div>Created by Gabriel Barrios</div>
-                    <a href="https://github.com/gbarrios212" className="devLink"><i className="fab fa-github devLink"> GitHub</i></a>
-                    <a href="https://www.linkedin.com/in/gabriel-antonio-barrios" className="devLink"><i className="fab fa-linkedin devLink"> LinkedIn</i></a>
+                    <a href="https://github.com/gbarrios212" className="devLink"><i className="fab fa-github devLink"> </i>  GitHub</a>
+                    <a href="https://www.linkedin.com/in/gabriel-antonio-barrios" className="devLink"><i className="fab fa-linkedin devLink"> </i>  LinkedIn</a>
                     <a href="https://angel.co/gabriel-barrios-2" className="devLink"><i className="fab fa-angellist devLink"></i>    AngelList</a>
                     <a href="https://gbarrios.com/" className="devLink2"><div className="portfolio-logo"></div>Portfolio</a>
                 </div>
