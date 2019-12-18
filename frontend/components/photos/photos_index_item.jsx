46e8c984a18b8c69e5c7f87a0ed38dcefba42fe2
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-12-18 15:59:01 -0500

    Add link to photo show page on followed photos

--- frontend/components/photos/photos_index_item.jsx
+++ frontend/components/photos/photos_index_item.jsx
@@ -6,39 +6,33 @@ const PhotosIndexItem = (props) => {
     return (
         
             <div className="follow-items-list-items">
-
                 <Link to={`/photos/${props.photo.id}`}>
                     <img src={props.photo.photoUrl} className="index-thumbnail" alt=""></img>
-                    {/* {photo.author.username} */}
                 </Link>
-                <div className="follow-modal">
-                    <Link to={`/users/${props.authorId}`}>
-                        <div className="follow-author-info">
-                            <div className="follow-avatar-container">
-                                <img src={props.authorPic} className="follow-avatar"/>
+                <Link to={`/photos/${props.photo.id}`}>
+                    <div className="follow-modal">
+                            <div className="follow-author-info">
+                                <div className="follow-avatar-container">
+                                    <img src={props.authorPic} className="follow-avatar"/>
+                                </div>
+                                <div className="follow-name">{props.author}</div>
                             </div>
-                            <div className="follow-name">{props.author}</div>
+                        <div className="follow-info">
+                            <div className="index-icons">
+                                <div className="index-icons-likes">
+                                    <i className="far fa-heart" id="index-heart"></i>{props.photo.like_ids.length}
+                                </div>
+                                <div className="index-icons-comments">
+                                    <i class="far fa-comment"></i>{props.photo.comment_ids.length}
+                                </div>
+                            </div>
+                            <p className="index-photo-title">
+                                {props.photo.name}
+                            </p>
                         </div>
-                    </Link>
-                    <div className="follow-info">
-                        <div className="index-icons">
-                            <div className="index-icons-likes">
-                                <i className="far fa-heart" id="index-heart"></i>{props.photo.like_ids.length}
-                            </div>
-                            <div className="index-icons-comments">
-                                <i class="far fa-comment"></i>{props.photo.comment_ids.length}
-                            </div>
-                        </div>
-                        <p className="index-photo-title">
-                            {props.photo.name}
-                        </p>
                     </div>
-                </div>
-              
-                 
-                
+                </Link>
             </div>
-        
     )
 }
 
