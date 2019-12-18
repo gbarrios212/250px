945047c40a5230142287537c732cdcd1700bce38
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-12-18 13:31:44 -0500

    Add styling to home page

--- frontend/components/photos/featured_photos_index_item.jsx
+++ frontend/components/photos/featured_photos_index_item.jsx
@@ -6,23 +6,27 @@ const FeaturedPhotosIndexItem = ({ photo }) => {
   return (
     <div className="featured-photo-item">
       <Link to={`/photos/${photo.id}`}>
+        <div className="featured-modal"></div>
+      </Link>
         <img id="featured-thumbnail" src={photo.photoUrl} alt=""></img>
         {/* {photo.author.username} */}
-      </Link>
-      <div className="index-icons">
-        <div className="index-icons-likes">
-          <i className="far fa-heart" id="index-heart"></i>
-          {photo.like_ids.length}
-          {/* <div className="index-likes">
-                            {photo.liker_ids.length}
-                        </div> */}
+      <div className="featured-info">
+
+        <div className="featured-index-icons">
+            <div className="featured-index-icons-likes">
+            <i className="far fa-heart" id="ft-index-heart"></i>
+            {photo.like_ids.length}
+            {/* <div className="index-likes">
+                                {photo.liker_ids.length}
+                            </div> */}
+            </div>
+            <div className="featured-index-icons-comments">
+            <i className="far fa-comment" id="ft-index-comment"></i>
+            {photo.comment_ids.length}
+            </div>
         </div>
-        <div className="index-icons-comments">
-          <i class="far fa-comment"></i>
-          {photo.comment_ids.length}
-        </div>
+        <p className="featured-index-photo-title">{photo.name}</p>
       </div>
-      <p className="index-photo-title">{photo.name}</p>
     </div>
   );
 };
