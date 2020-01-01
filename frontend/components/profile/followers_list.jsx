e5865e01c336458f98a453bd34e7b0def0448371
Gabriel Barrios <gabrielbarrios212@gmail.com>
2019-12-31 19:11:30 -0500

    Add styling to follower list modal

---
+++ frontend/components/profile/followers_list.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class FollowsList extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+           <div className="followers-list">
+               hello
+           </div> 
+        )
+    }
+}
+
+export default FollowsList;
--- frontend/components/profile/followers_list_container.jsx
+++ frontend/components/profile/followers_list_container.jsx
@@ -3,9 +3,13 @@ import { connect } from 'react-redux';
 import { closeModal } from '../../actions/modal_actions';
 
 const msp = (state, ownProps) => {
-
+    return ({})
 }
 
 const mdp = (dispatch) => {
-    closeModal: () => dispatch(closeModal())
+    return ({
+        closeModal: () => dispatch(closeModal())
+    })
 }
+
+export default connect(msp, mdp)(FollowersList);
